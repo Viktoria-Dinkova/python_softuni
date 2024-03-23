f0697122f0f4d2352429f02e6ca6c8b36c9eb583
Viktoria Dinkova <ViKiDi.work@gmail.com>
2024-03-23 19:20:34 +0200

    Upload data

---
+++ oop/inheritance/lab/05_hierarchical_inheritance/project/dog.py
@@ -0,0 +1,15 @@
+"""
+In a folder called project create three files: vehicle.py and car.py, and sports_car.py.
+In each file, create its corresponding class - Vehicle, Car, and SportsCar:
+•	Vehicle with a single method move() that returns: "moving..."
+•	Car with a single method drive() that returns: "driving..."
+•	SportsCar with a single method race() that returns: "racing...".
+SportsCar should inherit from Car and Car should inherit from Vehicle.
+"""
+from project.animal import Animal
+
+
+class Dog(Animal):
+
+    def bark(self):
+        return "barking..."
