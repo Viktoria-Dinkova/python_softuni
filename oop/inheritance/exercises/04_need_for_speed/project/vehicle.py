b76b0a01eaa2fd4b5e10f0abbe4380c865da4c2b
Viktoria Dinkova <ViKiDi.work@gmail.com>
2024-03-28 17:52:46 +0200

    Upload data

---
+++ oop/inheritance/exercises/04_need_for_speed/project/vehicle.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    DEFAULT_FUEL_CONSUMPTION: float = 1.25
+
+    def __init__(self, fuel: float, horse_power: int):
+        self.fuel = fuel
+        self.horse_power = horse_power
+        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
+
+    def drive(self, kilometers):
+        needed_fuel = self.fuel_consumption * kilometers
+
+        if needed_fuel <= self.fuel:
+            self.fuel -= needed_fuel
