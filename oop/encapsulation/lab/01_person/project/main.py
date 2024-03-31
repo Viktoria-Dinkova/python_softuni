c8ae5559750609ca06a4cb0f26c349544369193b
Viktoria Dinkova <ViKiDi.work@gmail.com>
2024-03-31 19:24:18 +0300

    Upload data

---
+++ oop/encapsulation/lab/01_person/project/main.py
@@ -0,0 +1,5 @@
+from project.person import Person
+
+person = Person("George", 32)
+print(person.get_name())
+print(person.get_age())
---
+++ oop/encapsulation/lab/02_mammal/project/main.py
@@ -0,0 +1,6 @@
+from project.mammal import Mammal
+
+mammal = Mammal("Dog", "Domestic", "Bark")
+print(mammal.make_sound())
+print(mammal.get_kingdom())
+print(mammal.info())
