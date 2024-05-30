3fcf52756506778242fa4438db5afd05334ec104
Viktoria Dinkova <ViKiDi.work@gmail.com>
2024-05-30 19:24:35 +0300

    Upload data

---
+++ oop/exams/exam_20220418/project/movie_specification/action.py
@@ -0,0 +1,21 @@
+from project.movie_specification.movie import Movie
+
+
+class Action(Movie):
+    AGE_RESTRICTION = 12
+
+    def __init__(self, title: str, year: int, owner, age_restriction: int = AGE_RESTRICTION):
+        super().__init__(title, year, owner, age_restriction)
+
+    @property
+    def age_restriction(self):
+        return self.__age_restriction
+
+    @age_restriction.setter
+    def age_restriction(self, value):
+        if value < self.AGE_RESTRICTION:
+            raise ValueError(f"Action movies must be restricted for audience under {self.AGE_RESTRICTION} years!")
+        self.__age_restriction = value
+
+    def details(self) -> str:
+        return f"Action - Title:{self.title}, Year:{self.year}, Age restriction:{self.age_restriction}, Likes:{self.likes}, Owned by:{self.owner.username}"
