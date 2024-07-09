cf01ba62a14763520ddf9fdb212e86be01705d42
Viktoria Dinkova <ViKiDi.work@gmail.com>
2024-07-09 21:01:24 +0300

    Upload data

---
+++ oop/exams/exam_20231209/project/fish/deep_sea_fish.py
@@ -0,0 +1,17 @@
+from project.fish.base_fish import BaseFish
+
+
+class DeepSeaFish (BaseFish):
+
+    @property
+    def get_time_to_catch(self):
+        return 180
+
+    def __init__(self, name: str, points: float):
+        super().__init__(name, points, self.get_time_to_catch)
+
+    def fish_details(self):
+        return f"{self.__class__.__name__}: " \
+               f"{self.name} [Points: " \
+               f"{self.points}, " \
+               f"Time to Catch: {self.time_to_catch} seconds]"
