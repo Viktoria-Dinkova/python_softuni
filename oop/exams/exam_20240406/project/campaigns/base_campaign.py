7cbed4818fabc0dd73e8bd7ef3b232421bed1650
Viktoria Dinkova <ViKiDi.work@gmail.com>
2024-07-22 06:40:51 +0300

    Upload data

---
+++ oop/exams/exam_20240406/project/campaigns/base_campaign.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class BaseCampaign(ABC):
+    UNIQUE_CAMPAIGN_ID = []
+
+    def __init__(self, campaign_id: int, brand: str, budget: float, required_engagement: float):
+        self.campaign_id = campaign_id
+        self.brand = brand
+        self.budget = budget
+        self.required_engagement = required_engagement
+        self.approved_influencers = []
+
+    @property
+    def campaign_id(self):
+        return self.__campaign_id
+
+    @campaign_id.setter
+    def campaign_id(self, value):
+        if not (value > 0):
+            raise ValueError("Campaign ID must be a positive integer greater than zero.")
+        if value in self.UNIQUE_CAMPAIGN_ID:
+            raise ValueError(f"Campaign with ID {value} already exists. Campaign IDs must be unique.")
+
+        self.__campaign_id = value
+
+    @abstractmethod
+    def check_eligibility(self, engagement_rate: float) -> bool:
+        pass
